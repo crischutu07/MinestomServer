f6456d709500555c664bbfd9956af5e0d2261fba
crischutu07 <cris@crischutu07.is-a.dev>
2026-08-02 01:21:24 +0700

    delete bad comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,7 +16,6 @@ repositories {
 
 dependencies {
   implementation("net.minestom:minestom:2026.07.22-26.2")
-  // this is how you get rid of that annoying logger warning
   implementation("org.slf4j:slf4j-simple:2.0.9")
 }
 
