e199eaf5d5e57d195c6c2ec96baa42918c628348
crischutu07 <cris@crischutu07.is-a.dev>
2026-08-02 01:17:13 +0700

    improve gradle build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,50 @@
 plugins {
-    application
+  java
+  application
+  id("com.gradleup.shadow") version "9.6.1"
 }
 
+group = "dev.crischutu07"
+
 application {
-    mainClass.set("dev.crischutu07.MinestomServer.Main");
+  mainClass.set("${project.group}.MinestomServer.Main");
 }
 
 repositories {
-    mavenCentral()
+  mavenCentral()
 }
 
 dependencies {
-    implementation("net.minestom:minestom:2026.07.22-26.2")
-    // this is how you get rid of that annoying logger warning
-    implementation("org.slf4j:slf4j-simple:2.0.9")
+  implementation("net.minestom:minestom:2026.07.22-26.2")
+  // this is how you get rid of that annoying logger warning
+  implementation("org.slf4j:slf4j-simple:2.0.9")
 }
+
+tasks {
+  jar {
+    enabled = false
+  }
+
+  startScripts {
+    enabled = false
+  }
+  
+  build {
+    dependsOn(shadowJar)
+  }
+  
+  shadowJar {
+    manifest {
+      attributes["Main-Class"] = "${project.group}.MinestomServer.Main"
+    }
+  }
+}
+
+java {
+  toolchain.languageVersion = JavaLanguageVersion.of(25)
+}
+
+tasks.withType<JavaCompile> {
+  options.encoding = "UTF-8"
+}
+
